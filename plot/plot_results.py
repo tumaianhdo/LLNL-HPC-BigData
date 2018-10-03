a660e1600d4539f21cd09118a86f97536ea40a2a
Tu Mai Anh Do <do7@catalyst159.llnl.gov>
2018-10-03 14:34:11 -0700

    Fix chart plot

--- plot/plot_results.py
+++ plot/plot_results.py
@@ -62,14 +62,45 @@ label = ['6GB', '60GB', '600GB']
 plt.figure()
 plt.subplot()
 # x = np.arange(0,6,2.5)
+res = 100
 x = [6, 60, 600]
+# x1 = np.linspace(np.min(x),np.max(x),res)
 
+# variables = [total_lustre, total_nvm]
+
+# for var in variables:
+# 	plt.figure()
+# 	y1 = np.linspace(var[0],var[-1],res)
+# 	print var
+# 	plt.plot(x1, y1, marker='x')
+# 	plt.plot(x, var, marker='o')
+# 	plt.xlim([0,750])
+
+# plt.figure()
 plt.plot(x, total_lustre, label='Total_lustre', marker='o')
 plt.plot(x, total_nvm, label='Total_nvm', marker='o')
 plt.plot(x, sim_lustre, label='Simulation_lustre', marker='o')
 plt.plot(x, sim_nvm, label='Simulation_nvm', marker='o')
 plt.plot(x, ana_lustre, label='Analysis_lustre', marker='o')
 plt.plot(x, ana_nvm, label='Analysis_nvm', marker='o')
+
+cc = 0
+for val in x:
+	plt.plot([val]*2, [0, total_lustre[cc]], 'k--')
+	cc += 1
+
+plt.xticks(list(plt.xticks()[0]) + x)
+plt.xlim(0,650)
+plt.ylim(0,np.max(total_lustre)+100)
+# plt.show()
+
+
+# plt.plot(x, total_lustre, label='Total_lustre', marker='o')
+# plt.plot(x, total_nvm, label='Total_nvm', marker='o')
+# plt.plot(x, sim_lustre, label='Simulation_lustre', marker='o')
+# plt.plot(x, sim_nvm, label='Simulation_nvm', marker='o')
+# plt.plot(x, ana_lustre, label='Analysis_lustre', marker='o')
+# plt.plot(x, ana_nvm, label='Analysis_nvm', marker='o')
 # plt.bar(x-0.15, clean_in_lustre, width=0.2, bottom=sum_in_lustre, label='in-situ + lustre Clean up')
 # plt.bar(x-0.15, ana_in_lustre, width=0.2, bottom=syn_in_lustre, label='in-situ + lustre Cumulative Analysis')
 # plt.bar(x-0.15, clean_in_lustre, width=0.2, bottom=sum_in_lustre, label='in-situ + lustre Clean up')
@@ -83,11 +114,11 @@ plt.plot(x, ana_nvm, label='Analysis_nvm', marker='o')
 # plt.bar(x+0.85, syn_post_lustre, width=0.2, label='post-processing + lustre Synthetic Simulation')
 # plt.bar(x+0.85, ana_post_lustre, width=0.2, bottom=syn_post_lustre, label='post-processing + lustre Cumulative Analysis')
 # plt.bar(x+0.85, clean_post_lustre, width=0.2, bottom=sum_post_lustre, label='post-processing + lustre Clean up')
-plt.xlabel('Data Size')
+plt.xlabel('Data Size (GB)')
 plt.title('8 nodes')
 plt.ylabel('TIME (s)')
 plt.legend(loc='best')
-plt.xticks(x, label)
+# plt.xticks(x, label)
 
 # plt.subplot(1,2,2)
 
